152affbda5823d26febf9d37fe049751a22edfed
yukihiro hara <yukihr@gmail.com>
2015-02-06 00:23:03 +0900

    use bazaar for nfcpy installation

--- setup.py
+++ setup.py
@@ -3,69 +3,34 @@
 from setuptools import setup
 from setuptools.command.install import install
 import os
-import urllib2
-import tarfile
 import shutil
+from subprocess import call
 
 
 class NfcpyPackage:
     'Nfcpy package manipulation.'
 
-    TARFILE = 'nfcpy-%s.tar.gz'
-    SRC_URL = 'https://launchpad.net/nfcpy/%s/%s/+download/%s'
+    BRANCH_URL = 'lp:nfcpy'
 
-    def __init__(self, version, dist_dir, cache_dir):
-        self.version = version
-        self.version_to_minor = '.'.join(version.split('.')[0:2])
-        self.src_tarball = NfcpyPackage.TARFILE % version
-        self.src_url = NfcpyPackage.SRC_URL \
-            % (self.version_to_minor,
-               self.version,
-               self.src_tarball)
+    def __init__(self, dist_dir):
         self.dist_dir = dist_dir
-        self.src_true_dirname = self.src_tarball[:-len('.tar.gz')]
-        self.src_dirname = 'nfcpy'
-        self.src_true_path = os.path.join(dist_dir, self.src_true_dirname)
+        self.src_true_path = os.path.join(dist_dir, 'nfcpy')
         self.lib_true_path = os.path.join(self.src_true_path, 'nfc')
         self.lib_path = os.path.join(dist_dir, 'nfc')
-        self._cache_dir = cache_dir
-        self.cache_file = os.path.join(self.cache_dir, self.src_tarball)
 
-    @property
-    def cache_dir(self):
-        'Get cache directory for project making it if not exists.'
-        d = self._cache_dir
-        if not os.path.exists(d):
-            os.makedirs(d)
-        return d
-
-    @property
-    def tarball(self):
-        f = self.cache_file
-        if not os.path.exists(f):
-            self.download(self.src_url, f)
-        return f
-
-    def download(self, src_url, dist_path):
-        'Get nfcpy source tarball.'
-        response = urllib2.urlopen(src_url)
-        blob = response.read()
-        file = open(dist_path, 'w')
-        file.write(blob)
-        file.close
-
-    def extract_src(self, tarball_path, dist_path):
-        'Extract nfcpy source.'
-        tar = tarfile.open(tarball_path)
-        tar.extractall(dist_path)
-        tar.close()
+    def branch_repo(self, dist_dir):
+        pwd = os.getcwd()
+        os.chdir(dist_dir)
+        call(['bzr', 'branch', NfcpyPackage.BRANCH_URL])
+        os.chdir(pwd)
 
     def clean(self):
-        shutil.rmtree(self.cache_file)
+        shutil.rmtree(self.src_true_path)
 
     def install(self):
         'Install nfcpy.'
-        self.extract_src(self.tarball, self.dist_dir)
+        self.branch_repo(self.dist_dir)
+        os.unlink(self.lib_path)
         os.symlink(self.lib_true_path, self.lib_path)
 
 
@@ -73,14 +38,11 @@ class CustomInstallCommand(install):
     'Customized setuptools install command.'
 
     ROOT = os.path.dirname(os.path.abspath(__file__))
-    TMP_DIR = os.path.join(ROOT, 'tmp')
     SRC_DIR = os.path.join(ROOT, 'src')
 
     def run(self):
         nfcpy = NfcpyPackage(
-            version='0.9.1',
-            dist_dir=CustomInstallCommand.SRC_DIR,
-            cache_dir=CustomInstallCommand.TMP_DIR
+            dist_dir=CustomInstallCommand.SRC_DIR
         )
         nfcpy.install()
         install.run(self)
