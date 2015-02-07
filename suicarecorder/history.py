c24e6c24b6cfcfd4aced6d5c70029eeb3a191e95
yukihiro hara <yukihr@gmail.com>
2015-02-08 04:13:25 +0900

    update history data

--- suicarecorder/history.py
+++ suicarecorder/history.py
@@ -4,21 +4,61 @@ import struct
 import station
 import datetime
 
+DEFAULT_CONSOLE = u'不明端末'
+
 CONSOLES = {
-    0x03: u'精算機',
-    0x04: u'携帯型端末',
-    0x05: u'車載端末',
-    0x12: u'券売機',
-    0x16: u'改札機',
-    0x1c: u'乗継精算機',
-    0xc8: u'自販機'
+    3: u'精算機',
+    4: u'携帯型端末',
+    5: u'車載端末',
+    7: u'券売機',
+    8: u'券売機',
+    9: u'入金機',
+    18: u'券売機',
+    20: u'券売機等',
+    21: u'券売機等',
+    22: u'改札機',
+    23: u'簡易改札機',
+    24: u'窓口端末',
+    25: u'窓口端末',
+    26: u'改札端末',
+    27: u'携帯電話',
+    28: u'乗継精算機',
+    29: u'連絡改札機',
+    31: u'簡易入金機',
+    70: u'VIEW ALTTE',
+    72: u'VIEW ALTTE',
+    199: u'物販端末',
+    200: u'自販機'
 }
 
+DEFAULT_PROCESS = u'不明処理'
+
 PROCESSES = {
-    0x01: u'運賃支払',
-    0x02: u'チャージ',
-    0x0f: u'バス',
-    0x46: u'物販'
+    1: u'運賃支払(改札出場)',
+    2: u'チャージ',
+    3: u'券購(磁気券購入)',
+    4: u'精算',
+    5: u'精算 (入場精算)',
+    6: u'窓出 (改札窓口処理)',
+    7: u'新規 (新規発行)',
+    8: u'控除 (窓口控除)',
+    13: u'バス (PiTaPa系)',
+    15: u'バス (IruCa系)',
+    17: u'再発 (再発行処理)',
+    19: u'支払 (新幹線利用)',
+    20: u'入A (入場時オートチャージ)',
+    21: u'出A (出場時オートチャージ)',
+    31: u'入金 (バスチャージ)',
+    35: u'券購 (バス路面電車企画券購入)',
+    70: u'物販',
+    72: u'特典 (特典チャージ)',
+    73: u'入金 (レジ入金)',
+    74: u'物販取消',
+    75: u'入物 (入場物販)',
+    198: u'物現 (現金併用物販)',
+    203: u'入物 (入場現金併用物販)',
+    132: u'精算 (他社精算)',
+    133: u'精算 (他社入場精算)'
 }
 
 DEFAULT_FORMAT = u'''
@@ -58,8 +98,8 @@ class History(object):
         le = cls.process_as_little_endian(block)
 
         values = {
-            'console': CONSOLES.get(be[0], u'不明端末'),
-            'process': PROCESSES.get(be[1], u'不明用途'),
+            'console': CONSOLES.get(be[0], DEFAULT_CONSOLE),
+            'process': PROCESSES.get(be[1], DEFAULT_PROCESS),
             'date': cls.date_from_bytes(be[3]),
             'balance': le[8],
             'entered_station': station.for_codes(be[4], be[5]),
